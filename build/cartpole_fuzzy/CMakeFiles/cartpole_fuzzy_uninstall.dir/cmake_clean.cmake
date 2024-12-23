af132e2b2c00dd819423f1bf4aa607845cc3a5cc
Enes Birlik <enesbirlik0@gmail.com>
2024-12-23 11:26:23 +0300

    gitignore updated: only src folder will be tracked

--- build/cartpole_fuzzy/CMakeFiles/cartpole_fuzzy_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cartpole_fuzzy_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cartpole_fuzzy_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
